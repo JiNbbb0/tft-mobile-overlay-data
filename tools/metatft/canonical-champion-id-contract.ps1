eb48cb4c8c652561173a3783b12c0eebadf7d82f
Koyuki <koyuki@local>
2026-09-24 08:59:55 +0900

    fix: require ordinal TFT champion identity matches

--- tools/metatft/canonical-champion-id-contract.ps1
+++ tools/metatft/canonical-champion-id-contract.ps1
@@ -1,25 +1,37 @@
 Set-StrictMode -Version Latest
 
+function Test-TftCanonicalChampionIdExact {
+    param(
+        [Parameter(Mandatory = $true)][hashtable]$CanonicalChampionIds,
+        [Parameter(Mandatory = $true)][string]$Id
+    )
+
+    foreach ($canonicalId in $CanonicalChampionIds.Keys) {
+        if ([StringComparer]::Ordinal.Equals([string]$canonicalId, $Id)) { return $true }
+    }
+    return $false
+}
+
 function New-MetaTftCanonicalChampionAliasIndex {
     param(
         [Parameter(Mandatory = $true)][hashtable]$CanonicalChampionIds,
         [Parameter(Mandatory = $true)][object[]]$MetaTftUnits
     )
 
-    $candidatesBySourceId = @{}
+    $candidatesBySourceId = [Collections.Generic.Dictionary[string, Collections.Generic.HashSet[string]]]::new([StringComparer]::Ordinal)
     $ambiguousSources = [Collections.Generic.HashSet[string]]::new([StringComparer]::Ordinal)
     foreach ($unit in $MetaTftUnits) {
         $canonicalTargets = [Collections.Generic.HashSet[string]]::new([StringComparer]::Ordinal)
         foreach ($assetName in @($unit.assetNames)) {
             $assetId = [string]$assetName
-            if ($CanonicalChampionIds.ContainsKey($assetId)) { [void]$canonicalTargets.Add($assetId) }
+            if (Test-TftCanonicalChampionIdExact -CanonicalChampionIds $CanonicalChampionIds -Id $assetId) { [void]$canonicalTargets.Add($assetId) }
         }
         $metaApiName = [string]$unit.apiName
-        if ($CanonicalChampionIds.ContainsKey($metaApiName)) { [void]$canonicalTargets.Add($metaApiName) }
+        if (Test-TftCanonicalChampionIdExact -CanonicalChampionIds $CanonicalChampionIds -Id $metaApiName) { [void]$canonicalTargets.Add($metaApiName) }
         if ($canonicalTargets.Count -ne 1) {
             if ($canonicalTargets.Count -gt 1) {
                 foreach ($sourceId in @([string]$unit.apiName) + @($unit.assetNames | ForEach-Object { [string]$_ })) {
-                    if ($sourceId -and -not $CanonicalChampionIds.ContainsKey($sourceId)) { [void]$ambiguousSources.Add($sourceId) }
+                    if ($sourceId -and -not (Test-TftCanonicalChampionIdExact -CanonicalChampionIds $CanonicalChampionIds -Id $sourceId)) { [void]$ambiguousSources.Add($sourceId) }
                 }
             }
             continue
@@ -27,7 +39,7 @@ function New-MetaTftCanonicalChampionAliasIndex {
 
         $canonicalId = @($canonicalTargets)[0]
         foreach ($sourceId in @($metaApiName) + @($unit.assetNames | ForEach-Object { [string]$_ })) {
-            if (-not $sourceId -or $CanonicalChampionIds.ContainsKey($sourceId)) { continue }
+            if (-not $sourceId -or (Test-TftCanonicalChampionIdExact -CanonicalChampionIds $CanonicalChampionIds -Id $sourceId)) { continue }
             if (-not $candidatesBySourceId.ContainsKey($sourceId)) {
                 $candidatesBySourceId[$sourceId] = [Collections.Generic.HashSet[string]]::new([StringComparer]::Ordinal)
             }
@@ -35,7 +47,7 @@ function New-MetaTftCanonicalChampionAliasIndex {
         }
     }
 
-    $index = @{}
+    $index = [Collections.Generic.Dictionary[string, string]]::new([StringComparer]::Ordinal)
     $ambiguousIds = [Collections.Generic.List[string]]::new()
     foreach ($sourceId in $candidatesBySourceId.Keys) {
         $targets = @($candidatesBySourceId[$sourceId])
@@ -57,10 +69,10 @@ function Resolve-MetaTftCanonicalChampionId {
     param(
         [Parameter(Mandatory = $true)][string]$SourceId,
         [Parameter(Mandatory = $true)][hashtable]$CanonicalChampionIds,
-        [Parameter(Mandatory = $true)][hashtable]$Aliases
+        [Parameter(Mandatory = $true)][System.Collections.IDictionary]$Aliases
     )
 
-    if ($CanonicalChampionIds.ContainsKey($SourceId)) { return $SourceId }
+    if (Test-TftCanonicalChampionIdExact -CanonicalChampionIds $CanonicalChampionIds -Id $SourceId) { return $SourceId }
     if ($Aliases.ContainsKey($SourceId)) { return [string]$Aliases[$SourceId] }
     throw "UNRESOLVED_CANONICAL_CHAMPION_ID raw=$SourceId"
 }
